7af2f90c9fe2b2c19674cd8ec05f1444c62b62e7
87al <87al@fresnou.org>
2022-10-13 11:23:44 -0700

    second commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res){
   res.render('index');
 });
 
+router.post('/card', function(req, res){
+  console.log(req.body.dateOfBirth)
+  res.render('card', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+    type: req.body.type,
+    dateOfBirth
+  })
+})
 module.exports = router;
